ae27cbed516987f88e74317f13834738cd8034dd
Netanel Sharbat <n.sharbat@gmail.com>
2019-12-13 22:55:16 +0200

    Netanel - Link the signUp page to loginPage

--- src/Authentication/logInPage.js
+++ src/Authentication/logInPage.js
@@ -145,7 +145,7 @@ const LogInSide = ({ history }) => {
                                 </Link>
                             </Grid>
                             <Grid item>
-                                <Link href="#" variant="body2">
+                                <Link href="./signUp" variant="body2">
                                     {"Don't have an account? Sign Up"}
                                 </Link>
                             </Grid>
