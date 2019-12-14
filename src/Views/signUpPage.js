26195bb94156bf610bbfe70de50c1e8edc1011e0
Netanel Sharbat <n.sharbat@gmail.com>
2019-12-14 16:25:27 +0200

    Netanel - breaking to different files

--- src/Views/signUpPage.js
+++ src/Views/signUpPage.js
@@ -1,10 +1,8 @@
-import React, {useCallback, useContext} from 'react';
+import React, {useCallback} from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -13,21 +11,10 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import app from '../Config/fire';
-import {withRouter, Redirect} from 'react-router';
-import { AuthContext } from "../Actions/auth";
+import {withRouter} from 'react-router';
+import Copyright from "../Common/Copyright";
 
-function Copyright() {
-    return (
-        <Typography variant="body2" color="textSecondary" align="center">
-            {'Copyright © '}
-            <Link color="inherit" href="https://material-ui.com/">
-                Your Website
-            </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
-        </Typography>
-    );
-}
+
 
 const useStyles = makeStyles(theme => ({
     paper: {
