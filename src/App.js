9bdf7f8c39df75304bbcb0d8cc140b2d68fd60ed
Netanel Sharbat <n.sharbat@gmail.com>
2019-12-16 23:15:05 +0200

    Netanel - added a new empty sidebar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Home from './Views/userHomePage';
+import Home from './Views/User/userHomePage';
 import logIn from './Views/loginPage';
 import signUp from './Views/signUpPage';
 import { AuthProvider } from "./Actions/auth";
