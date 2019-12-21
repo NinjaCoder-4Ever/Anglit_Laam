a64a3cbcea035d591565f3c9eb6a78f983c34c42
Netanel Sharbat <n.sharbat@gmail.com>
2019-12-21 19:05:56 +0200

    Netanel - signout - create a log out function and redirect out

--- src/Views/Student/myLessons.js
+++ src/Views/Student/myLessons.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useCallback, useContext } from "react";
+import firebase from '../../Config/fire';
+import {AuthContext} from "../../Actions/auth";
+import {withRouter, Redirect} from 'react-router-dom';
 
 
 
-const myLessonsPage = () =>{
+const MyLessonsPage = ({ history }) => {
+
+    const handleLogout = () => {
+            try {
+                 firebase
+                    .auth()
+                    .signOut()
+                history.push("/login");
+            } catch (error) {
+                alert(error);
+            }
+    }
+
     return (
         <div>
             <div className="container">
                 <h2 className="center">My Lessons Page</h2>
-
+                <button onClick={handleLogout} >Sign out </button>
             </div>
         </div>
     );
 };
-export default myLessonsPage;
+export default withRouter (MyLessonsPage);
