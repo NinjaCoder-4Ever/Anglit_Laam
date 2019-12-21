a027637dfbcb63720e81951b07fed0cc59ba9942
Netanel Sharbat <n.sharbat@gmail.com>
2019-12-21 14:37:48 +0200

    Netanel - Student - create sidebar, add new pages, create routing, smart load of components

---
+++ src/Views/Student/homePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <div className="container">
+                <h4 className="center">Home Page</h4>
+                <p>bla bla</p>
+            </div>
+        </div>
+    )
+}
+
+export default Home
