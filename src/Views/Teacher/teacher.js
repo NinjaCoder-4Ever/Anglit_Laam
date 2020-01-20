7b246bc4436d132374058677d4ceb9ba80c2e746
Netanel Sharbat <n.sharbat@gmail.com>
2020-01-20 20:24:27 +0200

    change location

--- src/Views/Teacher/teacher.js
+++ src/Views/Teacher/teacher.js
@@ -10,7 +10,7 @@ import Sidebar from "../../Components/Sidebar/Sidebar";
 import routes from "../../teacherRoutes";
 import styles from "../../Layouts/adminStyle";
 import logo from '../../assets/img/logo512.png';
-import image from '../../sidebarBackground.jpg';
+import image from '../../assets/img/sidebarBackground.jpg';
 
 
 let ps;
