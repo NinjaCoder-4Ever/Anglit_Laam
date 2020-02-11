76a0e5bc853b2c87c8cfc8418d0f9e7cf47ebb08
Netanel Sharbat <n.sharbat@gmail.com>
2020-02-12 01:19:27 +0200

    Net - fix links from side bar

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -418,14 +418,14 @@ class Sidebar extends React.Component {
         var brand = (
             <div className={logoClasses}>
                 <a
-                    href="https://www.creative-tim.com?ref=mdpr-sidebar"
+                    href="https://www.anglitlaam.com/"
                     target="_blank"
                     className={logoMini}
                 >
                     <img src={logo} alt="logo" className={classes.img + " " + classes.logoAL} />
                 </a>
                 <a
-                    href="https://www.creative-tim.com?ref=mdpr-sidebar"
+                    href="https://www.anglitlaam.com/"
                     target="_blank"
                     className={logoNormal}
                 >
